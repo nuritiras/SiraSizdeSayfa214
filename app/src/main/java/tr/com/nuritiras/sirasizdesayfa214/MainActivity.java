d2a577c7b330c623a52eaf583be4d2d7f7951117
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-03-03 19:33:23 +0300

    Sira Sizde Sayfa 214

---
+++ app/src/main/java/tr/com/nuritiras/sirasizdesayfa214/MainActivity.java
@@ -0,0 +1,41 @@
+package tr.com.nuritiras.sirasizdesayfa214;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextNumara,editTextAdSoyad;
+    Button buttonEkle;
+    ListView listViewListe;
+    ArrayList<Ogrenci> ogrenciArrayList= new ArrayList<>();
+    ArrayAdapter<Ogrenci> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ogrenciArrayList);
+        editTextNumara=findViewById(R.id.editTextNumara);
+        editTextAdSoyad=findViewById(R.id.editTextAdSoyad);
+        buttonEkle=findViewById(R.id.buttonEkle);
+        listViewListe=findViewById(R.id.listViewListe);
+        listViewListe.setAdapter(adapter);
+        buttonEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ogrenci ogrenci=new Ogrenci();
+                ogrenci.setNumara(Integer.parseInt(editTextNumara.getText().toString()));
+                ogrenci.setAdSoyad(editTextAdSoyad.getText().toString());
+                ogrenciArrayList.add(ogrenci);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/tr/com/nuritiras/sirasizdesayfa214/Ogrenci.java
@@ -0,0 +1,22 @@
+package tr.com.nuritiras.sirasizdesayfa214;
+
+public class Ogrenci {
+    private int numara;
+    private String adSoyad;
+
+    public int getNumara() {
+        return numara;
+    }
+
+    public void setNumara(int numara) {
+        this.numara = numara;
+    }
+
+    public String getAdSoyad() {
+        return adSoyad;
+    }
+
+    public void setAdSoyad(String adSoyad) {
+        this.adSoyad = adSoyad;
+    }
+}
